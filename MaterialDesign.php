2e38fb25015866a3a4617a35cd5a46d45990ed74
ProjetoMoodleIFPE <ProjetoMoodleIFPE@gmail.com>
2016-11-02 20:33:05 -0300

    Add files via upload

---
+++ MaterialDesign.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+<title>Administrador</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+<link rel="stylesheet" href="https://code.getmdl.io/1.2.0/material.green-light_green.min.css" />
+<script defer src="https://code.getmdl.io/1.2.0/material.min.js"></script>
+</head>
+
+<body>
+
+</body>
+</html>
